701e588763a1e9f343d983aabf7aec5006f70531
benjaminmweribaya <b3njaminbaya@gmail.com>
2025-03-28 13:45:02 +0300

    Added ThemeContext and LanguageContext for dark mode and translations

---
+++ client/src/context/LanguageContext.jsx
@@ -0,0 +1,22 @@
+import { createContext, useState, useEffect } from "react";
+import { translations } from "../utils/language";
+
+export const LanguageContext = createContext();
+
+export const LanguageProvider = ({ children }) => {
+  const [language, setLanguage] = useState(localStorage.getItem("language") || "en");
+
+  useEffect(() => {
+    localStorage.setItem("language", language);
+  }, [language]);
+
+  const translate = (key) => {
+    return translations[language][key] || key;
+  };
+
+  return (
+    <LanguageContext.Provider value={{ language, setLanguage, translate }}>
+      {children}
+    </LanguageContext.Provider>
+  );
+};
